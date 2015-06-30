88055c595594c031d0372fa51f58fa5f9376c3d7
chucshary <shary-princess@hotmail.com>
2015-06-29 22:52:25 -0500

    Unique Username

--- SociApp_Events/app/models/user.rb
+++ SociApp_Events/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-validates :name, presence: true
+validates :name, presence: true, uniqueness:true
 validates :email, presence: true
 validates :password, presence: true
 end
