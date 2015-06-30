ddf99ab1fe90467fba81edf8adb2db210ee47c29
chucshary <shary-princess@hotmail.com>
2015-06-29 22:40:38 -0500

    Validacion en Blanco

--- SociApp_Events/app/controllers/users_controller.rb
+++ SociApp_Events/app/controllers/users_controller.rb
@@ -17,7 +17,11 @@ class UsersController < ApplicationController
 		@user=User.new(name:params[:user][:name],
 			email:params[:user][:email],
 			password:params[:user][:password])
-	@user.save
-	redirect_to @user
+	if @user.save
+		redirect_to @user
+
+	else
+		render :new
+		end
 	end
 end
