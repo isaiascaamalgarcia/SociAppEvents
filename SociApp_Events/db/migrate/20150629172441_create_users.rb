9c197ceae00e37efdec464938209c2bc0bc2f48f
denisse1993 <denissechacon15@hotmail.com>
2015-07-01 17:35:17 -0500

    Session controller
    API Sessions Controller login and logout

--- SociApp_Events/db/migrate/20150629172441_create_users.rb
+++ SociApp_Events/db/migrate/20150629172441_create_users.rb
@@ -8,10 +8,10 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
      ## Token authenticatable
        	t.string   :confirmation_token
-		t.datetime :confirmed_at
-		t.datetime :confirmation_sent_at
-		t.string   :authentication_token
-	    add_index  :users, :authentication_token, :unique => true
+		    t.datetime :confirmed_at
+		    t.datetime :confirmation_sent_at
+		    t.string   :authentication_token
+	      add_index  :users, :authentication_token, :unique => true
 
 
     end
