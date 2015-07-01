ffb4ddf51065449d44d11cbd64c3f1531fda823f
denisse1993 <denissechacon15@hotmail.com>
2015-07-01 17:02:15 -0500

    inicio de controller sesion

--- SociApp_Events/app/models/user.rb
+++ SociApp_Events/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
 validates :name, presence: true, uniqueness:true
 validates :email, presence: true
 validates :password, presence: true
+devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+before_save :ensure_authentication_token
+attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+def skip_confirmation!
+  self.confirmed_at = Time.now
+end
+
 end
--- SociApp_Events/db/migrate/20150629172441_create_users.rb
+++ SociApp_Events/db/migrate/20150629172441_create_users.rb
@@ -1,11 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, :null => false, :default => ""
       t.string :email
       t.string :password
 
       t.timestamps
+     ## Token authenticatable
+       	t.string   :confirmation_token
+		t.datetime :confirmed_at
+		t.datetime :confirmation_sent_at
+		t.string   :authentication_token
+	    add_index  :users, :authentication_token, :unique => true
+
+
     end
   end
 end
