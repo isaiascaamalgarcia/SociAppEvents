4d260229e17d4912947a52635e7edc4127573685
denisse1993 <denissechacon15@hotmail.com>
2015-07-01 20:32:32 -0500

    Correccion
    Borre todo lo de session controller

--- SociApp_Events/db/migrate/20150629172441_create_users.rb
+++ SociApp_Events/db/migrate/20150629172441_create_users.rb
@@ -1,18 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, :null => false, :default => ""
+      t.string :name
       t.string :email
       t.string :password
 
       t.timestamps
-     ## Token authenticatable
-       	t.string   :confirmation_token
-		    t.datetime :confirmed_at
-		    t.datetime :confirmation_sent_at
-		    t.string   :authentication_token
-	      add_index  :users, :authentication_token, :unique => true
-
+     
 
     end
   end
