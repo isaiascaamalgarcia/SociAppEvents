2c36a5064103111f1be862b3529a05a62252b145
chucshary <shary-princess@hotmail.com>
2015-07-06 22:14:52 -0500

    Dashboard

--- SociAppEventsPlay/public/javascripts/postUserToken.js
+++ SociAppEventsPlay/public/javascripts/postUserToken.js
@@ -1,27 +1,3 @@
-/*$('#loginForm').on('submit', function(e){
-
-    var formulario = document.getElementById('loginForm');
-
-    var datos = {};
-    for(var i = 0; i < formulario.length; i++){
-        if(formulario[i].name){
-            datos[formulario[i].name] = formulario[i].value
-        }
-    }
-        e.preventDefault();
-    $.ajax({
-        url: "/accessTokens",
-        type: "POST",
-        data: JSON.stringify(datos),
-        dataType: "application/json",
-        success: function(data) {
-        console.log(data);
-        }
-
-    });
-
-});*/
-
 var $x = $('#save');
 
 $x.click(function(evt) {
