d873bea2aa1e31af454e52a47448163c04e5c0b8
denisse1993 <denissechacon15@hotmail.com>
2015-07-28 12:55:18 -0500

    clase zavala

--- SociAppEventsPlay/app/controllers/PhotoController.java
+++ SociAppEventsPlay/app/controllers/PhotoController.java
@@ -10,14 +10,17 @@ import java.util.Base64;
  * Created by Shary on 23/07/2015.
  */
 public class PhotoController extends Controller {
-    public Result createPhoto() {
-        //User user = new User();
+    public Result createPhoto(int userId, int evntId) {
+        //User user = new User(); recibir token, validar que el token exista, que el usuario este invitado al evento
         Photo photo = Json.fromJson(request().body().asJson(), Photo.class);
         //photo.setUser(user);
         //PAQUETE PLAY.LIBS.CODEC
         String base64=photo.getBase64();
         byte [] bytes= Base64.getDecoder().decode(base64);
         //byte [] bytesBase64.decode(base64);
+        //String random = usar UUID para generar nombre aleatorio
+        String extension = photo.getType();
+        String filename = rendom + "." + extension;
         photo.save();
         return ok(Json.toJson(photo));
     }
